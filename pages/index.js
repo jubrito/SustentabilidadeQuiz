c4fdfe2d3247a16eee14e22ecf8aaa1f998f15d8
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2021-09-06 17:52:14 -0300

    Changes image metatag

--- pages/index.js
+++ pages/index.js
@@ -70,6 +70,7 @@ export default function Home() {
         <meta property="og:locale" content="pt_BR"/>
         <meta property="og:type" content="website"/>
         <meta property="og:image"content="./cover.jpg"/>
+        <meta property="og:image"content="https://sustentabilidade-quiz-jubrito.vercel.app/cover.jpg"/>
         <meta property="og:url" content="https://sustentabilidade-quiz-jubrito.vercel.app"/>
         <meta name="description" content="O objetivo desse teste é te ajudar a encontrar oportunidades para deixar suas ações mais sustentáveis! O primeiro passo para transformar o mundo é entender os impactos das nossas ações e, ao mesmo tempo, descobrir novas alternativas."></meta>
         <meta name="google-site-verification" content="h4778lwuG2KPHy--S9BxnxUsFRSchljthm2INr8jO_U" />
